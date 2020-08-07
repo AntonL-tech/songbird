8f0a87f0d0a93012315c9d3136d2ecf1daf1b9c8
Anton <antonlashyk@gmail.com>
2020-08-07 12:21:51 +0300

    feat: add main app folder and app-header

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AppHeader = () => {
+  return (
+    <div className = "app-header">
+      <h1>SongBird</h1>
+      <h2>Score 5</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
