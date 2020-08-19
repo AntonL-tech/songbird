f76e193edbeef16746591e8b508b1de2b1d229b8
Anton <antonlashyk@gmail.com>
2020-08-19 16:20:02 +0300

    feat: add styles for randomBird component

--- src/components/app-randomBird/app-randomBird.js
+++ src/components/app-randomBird/app-randomBird.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import './app-randomBird.css'
+
 const RandomBird = ({randomBird, isRightAnswer}) => {
   const defaultBirdUrl = "https://www.publicdomainpictures.net/pictures/60000/nahled/bird-silhouette-blackbird.jpg"
   return (
-    <div>
-      <img src={isRightAnswer ? randomBird.image : defaultBirdUrl} alt="random-bird"/>
-      <div>
-        <h2>{isRightAnswer ? randomBird.name : '******'}</h2>
+    <div className="random-bird">
+      <img className="random-bird-image" src={isRightAnswer ? randomBird.image : defaultBirdUrl} alt="random-bird"/>
+      <div className="random-bird-content">
+        <h2 className="random-bird-title">{isRightAnswer ? randomBird.name : '******'}</h2>
         <audio controls>
           <source src={randomBird.audio} type="audio/mpeg"/>
         </audio>
